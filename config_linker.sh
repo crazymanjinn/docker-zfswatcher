4ada6b36e4f857c3fc76d4e0ab8b0b4bdfedf694
crazymanjinn <crazymanjinn@gmail.com>
2016-03-10 22:41:50 -0500

    fixed elif in config_linker.sh

--- config_linker.sh
+++ config_linker.sh
@@ -5,7 +5,7 @@ temp_config_path=/etc/zfs/zfswatcher.conf
 if [ -s $persist_config_path ]; then
   rm $temp_config_path
   ln -s $persist_config_path /etc/zfs/
-elif [ -e $persist_config_path -a ! -s $persist_config_path]; then
+elif [ -e $persist_config_path -a ! -s $persist_config_path ]; then
   mv $temp_config_path $persist_config_path
   ln -s $persist_config_path /etc/zfs
 fi
