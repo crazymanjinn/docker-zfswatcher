dc3d308fa08e4be545a3efcc2934f6e27494093c
crazymanjinn <crazymanjinn@gmail.com>
2016-03-10 23:21:59 -0500

    added start.sh

---
+++ start.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+persist_config_path=/zfswatcher_config/zfswatcher.conf
+temp_config_path=/etc/zfs/zfswatcher.conf
+
+if [ -s $persist_config_path ]; then
+  rm $temp_config_path
+  ln -s $persist_config_path /etc/zfs/
+elif [ -e $persist_config_path -a ! -s $persist_config_path ]; then
+  mv $temp_config_path $persist_config_path
+  ln -s $persist_config_path /etc/zfs
+fi
+
+exec zfswatcher
